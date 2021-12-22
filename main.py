06fb6e410cf6a9d995b6ec47f5a99ce1b28c575b
ms-tamara <tamarawanjira16@gmail.com>
2021-12-22 21:15:39 +0300

    insights

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def inventories_mimi():
             cur = conn.cursor()
             cur.execute("""SELECT * FROM  inventories""")
             rows = cur.fetchall()
-            print(rows)
+            # print(rows)
             # print (len(rows))
             # print(type(rows[0]))
             return render_template("inventories.html", rows=rows)
@@ -52,7 +52,7 @@ def sales():
     cur = conn.cursor()
     cur.execute("""SELECT id,product_id,quantity,DATE(sales_date),product_name FROM sales """)
     x = cur.fetchall()
-    print(x)
+    # print(x)
     # return redirect(url_for('sales'))
     
     if request.method == "POST":
@@ -82,7 +82,7 @@ def viewsale(x):
     cur = conn.cursor()
     cur.execute("""SELECT id,product_id,quantity,DATE(sales_date),product_name FROM sales where product_id=%(product_id)s """,{"product_id":x})
     x = cur.fetchall()
-    print(x)
+    # print(x)
     return render_template('sales.html' ,rows=x)
 
 
@@ -106,12 +106,32 @@ def editsale():
     
 @app.route('/insights')
 def dashboard():
-    return render_template("insights.html")
+    cur = conn.cursor()
+    cur.execute("""SELECT (extract ('month' from sales.sales_date)), sum(sales.quantity * inventories.selling_price) FROM sales JOIN inventories ON sales.product_id=inventories.id group by sales_date,sales.quantity;""")
+    spm=cur.fetchall()
+    print (spm)
+# convert spm to how chartjs expects.....
+
+
+
+
+    # cur = conn.cursor()
+    # cur.execute("""SELECT  quantity,product_name FROM sales;""")
+    # sbp=cur.fetchall()
+    # print (sbp)
+
+
+    # cur = conn.cursor()
+    # cur.execute("""SELECT  quantity,product_name FROM sales;""")
+    # sbp=cur.fetchall()
+    # print (sbp)
+    
+    return render_template("insights.html", spm=spm)
+
 
 
 
 app.run(debug=True)
-
 
 
 
--- static/Javascript/main.js
+++ static/Javascript/main.js
@@ -2,3 +2,5 @@
 $(document).ready( function () {
     $('#example').DataTable();
 } );
+
+
